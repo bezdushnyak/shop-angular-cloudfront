4eb491b9072945c0f555cf28a5f1b488cbd3b7a5
Dmytro Bezdushnyak <d.bezdushnyak@gmail.com>
2024-04-09 17:51:30 +0300

    feat(task-5): changed env vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://473mx6q1g3.execute-api.eu-north-1.amazonaws.com/dev',
+    import: 'https://bh1fhp4fq2.execute-api.eu-north-1.amazonaws.com/dev',
+    bff: 'https://b3alpi271b.execute-api.eu-north-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
